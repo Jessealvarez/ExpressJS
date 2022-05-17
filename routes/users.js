646a978e6d777839687591d4a7eca2574d994674
Jesse Alvarez <jesse@Jesses-MBP.home>
2022-05-17 14:44:57 -0400

    final commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var myName = 'Jesse';
+var movies = ['Fallen Angels', 'Drive My car', 'Nightmare Before christmas'];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/myname', function(req, res, next) {
+  res.send(myName);
+});
+
+router.get('/myfavoritemovies', function(req, res, next) {
+  res.json(movies);
 });
 
 module.exports = router;
